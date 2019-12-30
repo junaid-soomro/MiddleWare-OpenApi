cd51f67868ae1c333c76f76b0fcd593976c9d3d0
smshahrukh <shahrukh.2790@gmail.com>
2019-12-30 18:53:14 +0500

    ochestrator conflist resolve

--- routes/orchestrator.js
+++ routes/orchestrator.js
@@ -5,11 +5,7 @@ const requestAndLog = require('../utils');
 const orchBaseUrl = 'http://10.81.1.77:8002';
 
 const ORCH = {
-<<<<<<< HEAD
-    endpoint: ({ className, method }) => (`http://10.81.1.62:8002/backend_service?method=${className}.${method}`)
-=======
     endpoint: ({ className, method }) => (`${orchBaseUrl}/backend_service?method=${className}.${method}`)
->>>>>>> 5dae0e0afee692fefcf23113e4dfca9dece870ba
 }
 /* GET users listing. */
 router.route('/:class/:method')
@@ -21,24 +17,11 @@ router.route('/:class/:method')
             res.status(resp.response.status).json({ ...resp.data })
         }
         catch (e) {
-<<<<<<< HEAD
-            try {
-                next({
-                    status: e.response.status,
-                    message: e.data.body.message,
-                    errors: null
-                })
-            } catch (unknownException) {
-                next({ ...e });
-            }
-
-=======
             next({
                 status: e,
                 message: e,
                 errors: null
             })
->>>>>>> 5dae0e0afee692fefcf23113e4dfca9dece870ba
         }
     })
     .post(async (req, res, next) => {
